4c4a2d0acfd8493d92eaa3da20b62a114668cc76
DuiLego <cesar.leal@solgeek.com>
2025-07-14 17:25:49 -0600

    :tada: Se inicio el proyecto y se finalizaron las actividades de la semana 2

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+
+    constructor(
+        private readonly carsService: CarsService
+    ) {}
+
+    @Get()
+    getAllCars() {
+        return this.carsService.findAll()
+    }
+
+    @Get(':id')
+    getCarById( @Param('id', ParseIntPipe) id: number) {
+        console.log({ id })
+
+        return this.carsService.findOneById(id)
+    }
+
+    @Post()
+    createCar( @Param('id', ParseIntPipe) id: number, @Body() request: any) {
+        return request
+    }
+
+    @Patch(':id')
+    updateCar( @Body() request: any) {
+        return request
+    }
+
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe) id: number) {
+        return id
+    }
+}
